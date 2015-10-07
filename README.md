9a9c05252e2636569107997426dee5d98a3a6707
Buck DeFore <bdefore@gmail.com>
2015-10-07 14:23:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# express-jwt-proxy
+
+An express middleware to route login requests to an external authentication server and store JWT's and add them to headers en route to an external service.
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,126 @@
+import bodyParser from 'body-parser';
+import httpProxy from 'http-proxy';
+import request from 'request';
+import session from 'express-session';
+
+const options = {
+  jwtClientId: undefined,
+  jwtClientSecret: undefined,
+  authenticationEndpoint: undefined,
+  apiHost: undefined,
+  apiPort: 80,
+  apiPrefix: 'api',
+  debug: false
+};
+
+let proxy = undefined;
+
+function debug(...args) {
+  if (options.debug) {
+    console.log(args);
+  }
+}
+
+function setupProxy(options) {
+  proxy = httpProxy.createProxyServer({
+    target: options.apiHost + ':' + options.apiPort,
+    changeOrigin: true // otherwise heroku claims app doesn't exist: http://stackoverflow.com/a/6445766/583755
+  });
+
+  // https://github.com/nodejitsu/node-http-proxy/issues/839
+  proxy.on('proxyReq', function (proxyReq, req, res) {
+    const apiRequestPath = `/${options.apiPrefix}${proxyReq.path}`
+    debug('api request, going to path: ', apiRequestPath)
+    proxyReq.path = apiRequestPath;
+  });
+
+  // added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
+  proxy.on('error', (error, req, res) => {
+    let json;
+    debug('proxy error', error);
+    if (!res.headersSent) {
+      res.writeHead(500, {'content-type': 'application/json'});
+    }
+
+    json = { error: 'proxy_error', reason: error.message };
+    res.end(JSON.stringify(json));
+  });
+}
+
+function configure(overrides) {
+  for(const key in overrides) {
+    options[key] = overrides[key]
+  }
+  debug('JWT Config options:', options);
+
+  options.sessionConfig = {
+    secret: options.sessionSecret,
+    resave: false,
+    saveUninitialized: false
+  }
+
+  setupProxy(options);
+}
+
+function login(req, res) {
+  console.log('making login request')
+  const form = {
+    grant_type: 'password',
+    email: req.body.user.email,
+    password: req.body.user.password
+  }
+  // client id and secret are not always required, only add if specified
+  if (options.jwtClientId && options.jwtClientSecret) {
+    form.jwtClientId = options.jwtClientId;
+    form.jwtClientSecret = options.jwtClientSecret;
+  }
+  request({
+    method: 'POST',
+    url: options.authenticationEndpoint,
+    form: form
+  }, (error, response, body) => {
+    response = JSON.parse(response.body);
+    if(!error && response.access_token) {
+      const token = response.access_token;
+      debug('auth returned access token', token)
+      req.session.token = token;
+      req.session.save((err) => {
+        if (err) {
+          debug('error saving session', err);
+        }
+      });
+      res.sendStatus(200);
+    } else {
+      debug('auth error', body);
+      res.sendStatus(401);
+    }
+  });
+}
+
+function logout(req, res) {
+  req.session.destroy((err) => {
+    debug('error destroying session', err);
+  });
+  res.sendStatus(200);
+}
+
+function apiProxy(req, res) {
+  if(req.session.token) {
+    debug('adding token:', req.session.token)
+    req.headers.authorization = `Bearer ${req.session.token}`
+  } else {
+    debug('no token to add')
+  }
+  proxy.web(req, res);
+};
+
+export default function (app, overrides) {
+
+  configure(overrides);
+
+  app.use(`/${options.apiPrefix}`, session(options.sessionConfig));
+  app.use(`/${options.apiPrefix}/login`, bodyParser.json());
+  app.post(`/${options.apiPrefix}/login`, login);
+  app.post(`/${options.apiPrefix}/logout`, logout);
+  app.use(`/${options.apiPrefix}`, apiProxy);
+}
