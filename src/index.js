c58c92d7e9ebaab68125bbd4ab5f59c978d3580f
Buck DeFore <bdefore@gmail.com>
2015-11-09 09:33:31 -0800

    remove http-proxy in prep for refresh token support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import bodyParser from 'body-parser';
-import httpProxy from 'http-proxy';
 import request from 'request';
 import session from 'express-session';
 const RedisStore = require('connect-redis')(session);
@@ -15,38 +14,10 @@ const options = {
   debug: false
 };
 
-let proxy = undefined;
-
 function debug(...args) {
   if (options.debug) {
     console.log(args);
   }
-}
-
-function setupProxy(options) {
-  proxy = httpProxy.createProxyServer({
-    target: options.apiHost + ':' + options.apiPort,
-    changeOrigin: true // otherwise heroku claims app doesn't exist: http://stackoverflow.com/a/6445766/583755
-  });
-
-  // https://github.com/nodejitsu/node-http-proxy/issues/839
-  proxy.on('proxyReq', function (proxyReq, req, res) {
-    const apiRequestPath = `/${options.apiPrefix}${proxyReq.path}`
-    debug('api request, going to path: ', apiRequestPath);
-    proxyReq.path = apiRequestPath;
-  });
-
-  // added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
-  proxy.on('error', (error, req, res) => {
-    let json;
-    debug('proxy error', error);
-    if (!res.headersSent) {
-      res.writeHead(500, {'content-type': 'application/json'});
-    }
-
-    json = { error: 'proxy_error', reason: error.message };
-    res.end(JSON.stringify(json));
-  });
 }
 
 function configure(overrides) {
@@ -54,8 +25,40 @@ function configure(overrides) {
     options[key] = overrides[key]
   }
   debug('JWT Config options:', options);
+}
 
-  setupProxy(options);
+function refreshAuth(req, res) {
+  const form = {
+    grant_type: 'refresh_token',
+    refresh_token: req.session.refresh_token
+  }
+  request({
+    method: 'POST',
+    url: options.authenticationEndpoint,
+    form: form
+  }, (error, response, body) => {
+    debug('refreshAuth error?', error);
+    handleAuthResponse(error, response, body, req, res);
+  });
+}
+
+function handleAuthResponse(error, response, body, req, res) {
+  response = JSON.parse(response.body);
+  if(!error && response.access_token) {
+    req.session.access_token = response.access_token;
+    req.session.refresh_token = response.refresh_token;
+    debug('auth returned access token', req.session.access_token);
+    debug('auth returned refresh token', req.session.refresh_token);
+    req.session.save((err) => {
+      if (err) {
+        debug('error saving session', err);
+      }
+    });
+    res.sendStatus(200);
+  } else {
+    debug('auth error', body);
+    res.sendStatus(401);
+  }
 }
 
 function login(req, res) {
@@ -74,21 +77,7 @@ function login(req, res) {
     url: options.authenticationEndpoint,
     form: form
   }, (error, response, body) => {
-    response = JSON.parse(response.body);
-    if(!error && response.access_token) {
-      const token = response.access_token;
-      debug('auth returned access token', token);
-      req.session.token = token;
-      req.session.save((err) => {
-        if (err) {
-          debug('error saving session', err);
-        }
-      });
-      res.sendStatus(200);
-    } else {
-      debug('auth error', body);
-      res.sendStatus(401);
-    }
+    handleAuthResponse(error, response, body, req, res);
   });
 }
 
@@ -100,16 +89,32 @@ function logout(req, res) {
 }
 
 function apiProxy(req, res) {
-  if(req.session.token) {
-    debug('adding token:', req.session.token);
-    req.headers.authorization = `Bearer ${req.session.token}`
+  let headers = {};
+  if(req.session.access_token) {
+    debug('adding token:', req.session.access_token);
+    headers.authorization = `Bearer ${req.session.access_token}`
   } else if(options.tokenOverride && process.env.NODE_ENV === 'development') {
     debug('overriding with token:', options.tokenOverride);
-    req.headers.authorization = `Bearer ${options.tokenOverride}`;
+    headers.authorization = `Bearer ${options.tokenOverride}`;
   } else {
     debug('no token to add');
   }
-  proxy.web(req, res);
+  headers['Content-Type'] = 'application/json';
+
+  const url = options.apiHost + ':' + options.apiPort + '/' + options.apiPrefix + req.url;
+  console.log('proxying to api at url:', url)
+  request({
+    method: req.method,
+    url: url,
+    headers: headers
+  }, (error, serviceResponse, body) => {
+    if (serviceResponse.statusCode === 401) {
+      res.sendStatus(401);
+    } else {
+      res.set(serviceResponse.headers);
+      res.json(body);
+    }
+  });
 };
 
 function setupSessionManager(app, options) {
@@ -130,7 +135,7 @@ function setupSessionManager(app, options) {
 }
 
 function addSessionStateHeader(req, res, next) {
-  if(req.session && req.session.token) {
+  if(req.session && req.session.access_token) {
     res.set('logged-in', true);
   } else {
     res.set('logged-in', false);
